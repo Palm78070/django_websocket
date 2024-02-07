245c4ad0e8381ed392a0d84262dcb574d89057b2
รัฐ ธรรมธัชอารี <rath@raths-MacBook-Pro.local>
2024-02-07 14:41:38 +0700

    commit2 add form to django

--- chat/consumers.py
+++ chat/consumers.py
@@ -23,3 +23,13 @@ class ChatConsumer(WebsocketConsumer):
 			'type':'connection_established',
 			'message':'You are now connected!!'
 		}))
+
+	def receive(self, text_data): #Called when receive msg from cli
+		#parse text_data(JSON format) into Python dict obj => use json.loads to convert json => dict obj
+		text_data_json = json.loads(text_data)
+
+		#retrieves the value associated with message key
+		#assumes that the received JSON data contains a key named 'messageee'
+		message = text_data_json['message']
+
+		print('Message:', message)
