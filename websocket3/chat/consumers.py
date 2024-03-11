d2e08dce1d4694d7897e27156210a8a56faa5b87
รัฐ ธรรมธัชอารี <rath@raths-MacBook-Pro.local>
2024-03-11 22:15:59 +0700

    commit12 can send reuest from postman websocket req

--- websocket3/chat/consumers.py
+++ websocket3/chat/consumers.py
@@ -4,29 +4,46 @@ from channels.db import database_sync_to_async
 from .models import ChatRoom, Chat
 from django.contrib.auth import get_user_model
 from django.utils.html import escape
+from django.urls import reverse
 
 User = get_user_model()
 user_ch_map = {}
 
 class ChatRoomConsumer(AsyncWebsocketConsumer):
 	async def connect(self):
+		await self.accept()
+		print("Connect to websocket!!!")
+		self.room_name = None
+		if not self.scope["user"].is_authenticated:
+		# Redirect to the login page
+			login_url = reverse("login-user")
+			redirect_message = {
+				"type": "redirect",
+				"url": login_url
+			}
+			#send json string back to cli
+			await self.send(text_data=json.dumps(redirect_message))
+			await self.close()
+			return
 		self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
 
 		#syntax of python to insert self.room_name to placeholder %s
+		print("After the condition!!!")
 		self.room_group_name = "chat_%s" % self.room_name
 		await self.channel_layer.group_add(
 			self.room_group_name,
 			self.channel_name
 		)
 
-		await self.accept()
 
 
 	async def disconnect(self, close_code):
-		await self.channel_layer.group_discard(
-			self.room_group_name,
-			self.channel_name
-		)
+		#hasattr check if the self obj has the attribute room_group_name
+		if hasattr(self, 'room_group_name'):
+			await self.channel_layer.group_discard(
+				self.room_group_name,
+				self.channel_name
+			)
 
 	async def save_massage(self, user, sender, message, room):
 		# To create and save an object in a single step, use the create() method.
